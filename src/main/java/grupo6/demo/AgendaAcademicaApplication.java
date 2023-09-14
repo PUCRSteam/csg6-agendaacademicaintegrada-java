61108249d30883766b08f059d48a8859b4b4bb8c
Arthur Viegas <arthurpviegas@gmail.com>
2023-09-14 18:02:05 -0300

    initalizing spring boot

---
+++ src/main/java/grupo6/demo/AgendaAcademicaApplication.java
@@ -0,0 +1,13 @@
+package grupo6.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaAcademicaApplication.class, args);
+	}
+
+}
